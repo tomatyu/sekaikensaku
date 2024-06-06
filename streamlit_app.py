a52c10bd47421f4dede457447d7105bea0aa9920
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-06 08:55:14 +0000

    8

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pandas as pd
 
 # フランスの座標
-france_coords = {'lat': 46.603354, 'lon': 1.888334}
+a = {'lat': 46.603354, 'lon': 1.888334}
 
 # ヴァルミーの座標
-valmy_coords = {'lat': 49.0136, 'lon': 4.9544}
+b = {'lat': 49.0136, 'lon': 4.9544}
+
+# メイエンの座標
+c = {"lat": 49.9929, "lon": 8.2473}
 
 # データフレームにフランスとヴァルミーの座標を追加
 df = pd.DataFrame([france_coords, valmy_coords])
