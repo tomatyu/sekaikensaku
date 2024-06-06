e182bf70380e569c2265dc747d91eaabe8e425c8
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-06 08:47:51 +0000

    ８

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(
-    columns=['lat', 'lon'])
+# フランスの座標
+france_coords = {'lat': 46.603354, 'lon': 1.888334}
 
+# ヴァルミーの座標
+valmy_coords = {'lat': 49.0136, 'lon': 4.9544}
+
+# データフレームにフランスとヴァルミーの座標を追加
+df = pd.DataFrame([france_coords, valmy_coords])
+
+# 地図上にピンを表示
 st.map(df)
