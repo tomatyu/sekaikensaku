68ff1db1271c1aa2775893432712a2576de41ece
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-06 08:35:37 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
+df = pd.DataFrame(
+    columns=['lat', 'lon'])
 
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    color='col4')
+st.map(df)
