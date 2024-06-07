cd498237eb5b471659fc115e99d3ebeb2e0630b1
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-07 14:41:48 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("$お試しだよ！$")
-a = st.text_input("以下の国を検索してね")
-st.write("例：アメリカ合衆国,中華人民共和国，日本国")
+a = st.text_input("検索")
+st.write("以下の中から検索してください")
+st.write("一覧：アメリカ合衆国,中華人民共和国，日本国")
 
-if st.write("日本国"):
-    st.write("我が国！")
+
