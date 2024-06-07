4c13dbaded7484d22bce78e6061c17b4c7403f6c
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-07 08:36:23 +0000

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,37 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import pydeck as pdk
 
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
 
-# フランスの座標
-a = {"lat": 46.603354, "lon": 1.888334}
-
-# ヴァルミーの座標
-b = {"lat": 49.0136, "lon": 4.9544}
-
-# 座標追加
-df = pd.DataFrame([a,b]) 
-
-# 線の始点と終点を指定
-start_point = [a]  # 東京駅の座標
-end_point = [b]      # 東京国際フォーラムの座標
-
-# 線を描画
-st.line(locations=[start_point, end_point], color='red').add_to(df)
-
-
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
