8cd8ec2aee600c1b01a7e063e38931206fc722bb
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-07 14:51:06 +0000

    ｃ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+a = ["アメリカ合衆国"]
+b = ["中華人民共和国"]
+c = ["日本国"]
 st.title("$お試しだよ！$")
-a = st.text_input("検索")
+kensaku = st.text_input("検索")
 st.write("以下の中から検索してください")
 st.write("一覧：アメリカ合衆国,中華人民共和国，日本国")
+
+if a:
+    st.write("自由の国！")
+if b:
+    st.write("中国料理うまい！")
+if c:
+    st.write("我が国！")
 
 
