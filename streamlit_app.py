1da7108d2db0e4f796d9383f695673196f8cfb4c
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-07 15:31:12 +0000

    ｆ

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ d = st.text_input("検索")
 st.write("以下の中から検索してください")
 st.write("一覧：アメリカ合衆国,中華人民共和国，日本国")
 
-if d == {a}:
+if d == a:
     st.write("自由の国！")
-elif d == {b}:
+elif d == b:
     st.write("中華料理うまい")
-elif d == {c}:
+elif d == c:
     st.write("我が国!")
 else:
     st.write("検索結果なし")
