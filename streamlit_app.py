52a5a0b95a7a2f2010413d2de3a95165ce3549f3
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 05:45:21 +0000

    ｘ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 st.title("$お試しだよ！$")
-a = ["アメリカ合衆国"]
-b = ["中華人民共和国"]
-c = ["日本国"]
-d = st.text_input("検索")
-st.write("以下の中から検索しなさい")
-
-if d == a:
+a = st.selectbox(
+    "検索",
+    ("アメリカ合衆国", "中華人民共和国", "日本"))
+st.write("以下の中から検索して下さい")
+st.write ("アメリカ合衆国", "中華人民共和国", "日本")
+if any("[v == アメリカ合衆国 for v in a]"):
     st.write("自由の国！")
-elif d == b:
+elif any("[v == 中華人民共和国 for v in a]"):
     st.write("中華料理うまい")
-elif d == c:
+elif any("[v == 日本 for v in a]"):
     st.write("我が国!")
 else:
     st.write("検索結果なし")
