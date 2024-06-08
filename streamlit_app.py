aa2d489a168adac3592897c706f0af7464ec9e58
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 07:02:20 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,19 @@ a = st.text_input("検索",)
 st.write("以下の中から検索して下さい")
 st.write ("アメリカ合衆国", "中華人民共和国", "日本")
 if any([f"{a}" == "アメリカ合衆国" for v in a]):
-    st.write("国名:アメリカ合衆国")
-    st.write("首都:ワシントンDC")
-    df = pd.DataFrame({
-    'lat': [38.5342],
-    'lot': [77.0212]
-    })
-    st.map(df)
+    # 国名と首都の表示
+   st.write("国名: アメリカ合衆国")
+   st.write("首都: ワシントンDC")
+
+    # データフレームの作成
+   usa_df = pd.DataFrame({
+    'lat': [38.8977],  # ワシントンDCの緯度
+    'lon': [-77.0365]  # ワシントンDCの経度
+   })
+ 
+     
+    # 地図へのピンの表示
+   st.map(usa_df)
 elif any([f"{a}" == "中華人民共和国" for v in a]):
     st.write("中華料理うまい")
 elif any([f"{a}" == "日本" for v in a]):
