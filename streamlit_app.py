e85b6a38d1494f87507e26ceeab280496f101bee
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 07:16:20 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,14 +16,36 @@ if any([f"{a}" == "アメリカ合衆国" for v in a]):
     'lat': [38.8977],  # ワシントンDCの緯度
     'lon': [-77.0365]  # ワシントンDCの経度
    })
+ # 地図へのピンの表示
+   st.map(usa_df,zoom = 1)
+elif any([f"{a}" == "中華人民共和国" for v in a]):
+     # 国名と首都の表示
+   st.write("国名: 中華人民共和国")
+   st.write("首都: 北京")
+
+    # データフレームの作成
+   cha_df = pd.DataFrame({
+    'lat': [39.9042],  # 北京の緯度
+    'lon': [116.4074]  # 北京の経度
+   })
  
      
     # 地図へのピンの表示
-   st.map(usa_df)
-elif any([f"{a}" == "中華人民共和国" for v in a]):
-    st.write("中華料理うまい")
+   st.map(cha_df,zoom = 1)
 elif any([f"{a}" == "日本" for v in a]):
-    st.write("我が国!")
+     # 国名と首都の表示
+   st.write("国名: 日本国")
+   st.write("首都: 東京")
+
+    # データフレームの作成
+   jap_df = pd.DataFrame({
+    'lat': [38.8977],  # 東京の緯度
+    'lon': [-77.0365]  # 東京の経度
+   })
+ 
+     
+    # 地図へのピンの表示
+   st.map(jap_df)
 else:
     st.write("検索結果なし")
 
