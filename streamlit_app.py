2bad043650e007000b53449f69eeaa09cd5065ed
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 05:58:35 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 st.title("$お試しだよ！$")
-a = st.selectbox(
-    "検索",
-    ("アメリカ合衆国", "中華人民共和国", "日本"))
+a = st.text_input("検索",)
 st.write("以下の中から検索して下さい")
 st.write ("アメリカ合衆国", "中華人民共和国", "日本")
 if any([f"{a}" == "アメリカ合衆国" for v in a]):
