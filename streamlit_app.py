a43d17f591042398074457dea310ca0b1fd6de19
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:57:15 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.title("$お試しだよ！$")
 a = st.text_input("検索",)
 st.write("以下の中から検索して下さい")
 st.write ("アメリカ合衆国", "中華人民共和国", "日本")
 if any([f"{a}" == "アメリカ合衆国" for v in a]):
-    st.write("自由の国！")
+    st.write("国名:アメリカ合衆国")
+    st.write("首都:ワシントンDC")
+    b = pd.DataFrame({
+    'lat': [38.5342],
+    'lot': [77.0212]
+    })
+    st.map(b)
 elif any([f"{a}" == "中華人民共和国" for v in a]):
     st.write("中華料理うまい")
 elif any([f"{a}" == "日本" for v in a]):
