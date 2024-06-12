72497ea0171e5093b70c5beb0e21a11f9f3e052d
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-12 07:54:50 +0000

    ｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,27 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
+import numpy as np
 
 # タイトルと説明
-st.title("世界検索")
-a = st.text_input("国を検索(対応していない国もあります)")
+st.title("国名、首都、座標検索")
+a = st.text_input("国名を検索")
 
 # データをロードする
 @st.cache
 def load_data():
-    return pd.read_excel("1234.xlsx")
+    return pd.read_excel("countries_data.xlsx")
 
-words_df = load_data()
+countries_df = load_data()
 
 # システム的なもの
-if any(words_df == a):
-    st.write(words_df[words_df == a])
+
+if any(countries_df["国名"] == a):
+    selected_country = countries_df[countries_df["国名"] == a].iloc[0]
+    st.write("国名:", selected_country["国名"])
+    st.write("首都:", selected_country["首都"])
+    st.write("座標:", selected_country["座標"])
+    
+    # st.map() を使用して座標を地図上に表示
+    st.map(selected_country["座標"])
 else:
     st.write("検索結果なし")
