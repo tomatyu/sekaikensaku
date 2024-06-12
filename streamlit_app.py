0904860bd34e29a7a7699d9be8ef6b16de303943
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-12 08:56:22 +0000

    i

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ def load_data():
 countries_df = load_data()
 
 # システム的なもの
-
+selected_country = countries_df[countries_df["国名"] == a]
+usa_df = pd.DataFrame({
+    'lat': [(selected_country["緯度"])],  # 緯度
+    'lon': [(selected_country["経度"])]  # 経度
+   })
 
 if any(countries_df["国名"] == a):
     st.write("国名:", selected_country["国名"])
@@ -24,50 +28,7 @@ if any(countries_df["国名"] == a):
     # st.map() を使用して座標を地図上に表示
     st.map(usa_df,zoom = 1)
 else:
-    st.write("検索結果なし")selected_country = countries_df[countries_df["国名"] == a].iloc[0]
-usa_df = pd.DataFrame({
-    'lat': [(selected_country["緯度"])],  # 緯度
-    'lon': [(selected_country["経度"])]  # 経度
-   })import streamlit as st
-import pandas as pd
-import numpy as np
-import openpyxl
-
-# タイトルと説明
-st.title("世界検索")
-a = st.text_input("国名を検索")
-
-# データをロードする
-@st.cache
-def load_data():
-    return pd.read_excel("2345.xlsx")
-
-countries_df = load_data()
-
-# システム的なもの
-selected_country = countries_df[countries_df["国名"] == a].iloc[0]
-usa_df = pd.DataFrame({
-    'lat': [(selected_country["緯度"])],  # 緯度
-    'lon': [(selected_country["経度"])]  # 経度
-   })
-
-if any(countries_df["国名"] == a):
-
-    stselected_country = countries_df[countries_df["国名"] == a].iloc[0]
-usa_df = pd.DataFrame({
-    'lat': [(selected_country["緯度"])],  # 緯度
-    'lon': [(selected_country["経度"])]  # 経度
-   }).write("国名:", selected_country["国名"])
-    st.write("首都:", selected_country["首都"])
-    
-    # st.map() を使用して座標を地図上に表示
-    st.map(usa_df,zoom = 1)
-else:
     st.write("検索結果なし")
-
-
-
-
 
 
 
