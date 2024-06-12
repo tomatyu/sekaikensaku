19400393f9e6b68849b1a8354900eb69480a0d58
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-12 07:23:59 +0000

    い

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,19 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-#　タイトルと説明
+# タイトルと説明
 st.title("世界検索")
 a = st.text_input("国を検索(対応していない国もあります)")
 
 # データをロードする
 @st.cache
-def b():
+def load_data():
     return pd.read_excel("1234.xlsx")
 
-words_df = b()
+words_df = load_data()
 
-#システム的なもの
-
-if any([f"{a}" == b for v in a]):
-    st.write(b)
+# システム的なもの
+if any(words_df["国"] == a):
+    st.write(words_df[words_df["国"] == a])
 else:
     st.write("検索結果なし")
