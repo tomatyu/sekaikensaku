e44328f323fa28c54e03a1804d8dd93ea29abb01
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-13 08:07:18 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ a = st.text_input("国名を検索(入ってない国がある可能性がある
 # データをロードする
 @st.cache
 def load_data():
-    return pd.read_excel("情報班 国一覧.xslx")
+    return pd.read_excel("12.xslx")
 
 countries_df = load_data()
 
