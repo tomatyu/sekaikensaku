5fdf2cbe60f99d625cfbd15ddbaf7cd821e1a396
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-19 15:34:31 +0000

    ｘ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,48 @@ if col1.button("国を検索する"):
     st.write("国を検索するボタンが押されました")
     # ここに国を検索するためのコードを追加する
 
-if col2.button("go"):
+if col2.button("go_button"):  # ボタンIDを"go_button"に変更
     st.title("国検索")
     st.write("好きな国を検索して、知識を見つけましょう！")
+    a = st.text_input("国名を検索してください（適用していない国もあります）")
 
+    # データをロードする
+    @st.cache
+    def load_data():
+        return pd.read_excel("17.xlsx")
+
+    countries_df = load_data()
+
+    # "go_button"ボタンが押されたときに以下の処理を実行
+    if col2.button("go_button"):
+        if any(countries_df["国名"] == a):
+            with st.spinner("検索中....."):
+                time.sleep(1)
+
+            selected_country = countries_df[countries_df["国名"] == a].iloc[0]
+            st.write("国名:", selected_country["国名"])
+            st.write("首都:", selected_country["首都"])import streamlit as st
+import pandas as pd
+import time
+
+# タイトルと説明
+st.title("世界検索")
+
+col1, col2, col3 = st.columns(3)
+
+if col1.button("国を検索する"):
+ st.title("国検索")
+ 
+
+if col2.button("go"):
+    st.write("konnitiha")
+
+if col3.button("hy"):
+    st.write("ey")
+
+
+
+st.write("好きな国を検索して、:red[知識] を見つけましょう！")
 a = st.text_input("国名を検索してください（適用していない国もあります）")
 
 # データをロードする
@@ -24,25 +62,19 @@ def load_data():
 
 countries_df = load_data()
 
-# col2の"go"ボタンが押されたときに以下の処理を実行
-if col2.button("go"):
-    if any(countries_df["国名"] == a):
-        with st.spinner("検索中....."):
-            time.sleep(1)
+# システム的なもの
+if any(countries_df["国名"] == a):
+    with st.spinner("検索中....."):
+        time.sleep(1)
 
-        selected_country = countries_df[countries_df["国名"] == a].iloc[0]
-        st.write("国名:", selected_country["国名"])
-        st.write("首都:", selected_country["首都"])
-        st.write("GDP:", selected_country["GDP"])
+    selected_country = countries_df[countries_df["国名"] == a].iloc[0]
+    st.write("国名:", selected_country["国名"])
+    st.write("首都:", selected_country["首都"])
+    st.write("GDP:", selected_country["GDP"])
+    
+    # st.map() を使用して座標を地図上に表示
+    st.map(pd.DataFrame({'lat': [selected_country["緯度"]], 'lon': [selected_country["経度"]]}), zoom=2)
+    st.write("概要:", selected_country["概要"])
 
-        # st.map() を使用して座標を地図上に表示
-        st.map(pd.DataFrame({'lat': [selected_country["緯度"]], 'lon': [selected_country["経度"]]}), zoom=2)
-        st.write("概要:", selected_country["概要"])
-
-    else:
-        st.write("検索結果なし")
-
-# col3の"hy"ボタンが押されたときに以下の処理を実行
-if col3.button("hy"):
-    st.write("ey")
-
+else:
+    st.write("検索結果なし")
