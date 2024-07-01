bf792bca673507f5e9fe4ac3b63e53772f319945
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-01 14:44:32 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,10 @@ import streamlit as st
 import pandas as pd
 import time
 import numpy as np
-import pyexpat as plt
 
 # タイトルと説明
 st.title("世界検索")
 
-countries = ["USA", "Japan", "China", "Russia", "UK"]
-gdp_data = pd.DataFrame(np.random.rand(5, 1) * 1000, columns=["GDP"], index=countries)
 
 
 col1, col2, col3 = st.columns(3)
@@ -24,15 +21,7 @@ if col2.button("go"):
 
 
 
- plt.figure(figsize=(10, 6))  # プロットのサイズを設定
- plt.bar(gdp_data.index, gdp_data["GDP"], color=['blue', 'green', 'red', 'purple', 'orange'])
- plt.title('GDP of Countries')
- plt.xlabel('Country')
- plt.ylabel('GDP (Billion $)')
- plt.grid(True)
-
-# Streamlitでグラフを表示
-st.pyplot()
+ 
 if col3.button("hy"):
     st.write("ey")
 
