908c3c19cb8bed0a620668a817b7fd5a22b241bf
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-09 13:57:03 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ elif tab == '国検索':
                 selected_country = selected_country.iloc[0]
                 st.write("国名:", selected_country["国名"])
                 st.write("首都:", selected_country["首都"])
-                st.write("GDP:", selected_country["GDP"],"兆ドル")
+                st.write("GDP:", selected_country["GDP"])
                 st.write("概要:", selected_country["概要"])
 
                 # 地図表示
@@ -63,6 +63,7 @@ elif tab == '国検索':
                 st.write("検索結果なし")
 
 elif tab == '国のGDP検索':
+    st.subheader('国のGDP検索')
     st.write("国名を入力してください（適用していない国もあります）")
     a = st.text_input("")
     if st.button('国のGDPを表示'):
