715446f1db9e9c6e2f18e23e9410976c3d583761
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-12 08:16:22 +0000

    ｔ

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,15 @@ st.title('Population Trends')
 st.write(data.head())
 
 # グラフの描画
-country = st.selectbox('国名を選択してください', data['国国国'].unique())
-filtered_data = data[data['国国国'] == country]
+country = st.selectbox('国名を選択してください', data['国名'].unique())
+filtered_data = data[data['国名'] == country]
 
 if not filtered_data.empty:
     plt.figure(figsize=(10, 6))
-    plt.plot(filtered_data['年'], filtered_data['人口'], marker='o')
-    plt.xlabel('年')
+    plt.plot(filtered_data.index, filtered_data['人口'], marker='o')  # indexを使って時系列の順序でプロット
+    plt.xlabel('データポイント')
     plt.ylabel('人口')
     plt.title(f'{country} の人口推移')
     st.pyplot(plt)
 else:
     st.write(f'{country} に関するデータはありません。')
-
