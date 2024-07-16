3b673e01eda4716cfd34fa10bec96a73b3d14d78
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-16 08:13:14 +0000

    ｌ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import numpy as np
-
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
-    import streamlit as st
-
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+ 
