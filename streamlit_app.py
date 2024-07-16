358311b99605361db3c38a5afa32e0725463e0ef
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-16 08:16:42 +0000

    ー

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+
 prompt = st.chat_input("Say something")
+
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
- 
+    paragraphs = prompt.split("\n")  # 改行文字でテキストを分割する
+    for paragraph in paragraphs:
+        st.write(f"User has sent the following prompt: {paragraph}")  # 各段落を新しい段落として表示する
