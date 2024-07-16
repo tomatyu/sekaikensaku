cc3cd374a2d0a6a96ac09739a2549940e360bf05
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-16 08:50:32 +0000

    う

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,26 +10,29 @@ def right_aligned_bubble(text):
                 padding: 10px;
                 text-align: right;
                 margin: 10px 0;
+                width: fit-content;
             }
             .bubble::after {
                 content: '';
                 position: absolute;
                 top: 50%;
-                right: -10px;
+                left: -10px;
                 border-style: solid;
-                border-width: 10px 0 10px 10px;
-                border-color: transparent transparent transparent #f0f0f0;
+                border-width: 10px 10px 10px 0;
+                border-color: transparent #f0f0f0 transparent transparent;
                 transform: translateY(-50%);
             }
         </style>
     '''
     bubble_html = f'''
         <div class="bubble">
-            {text}
+            <div style="text-align: left;">
+                {text}
+            </div>
         </div>
     '''
     st.markdown(bubble_style, unsafe_allow_html=True)
     st.markdown(bubble_html, unsafe_allow_html=True)
 
 # 使用例
-right_aligned_bubble("ここに吹き出しの内容が入ります。")
+right_aligned_bubble("ここに吹き出しの内容が入ります。この内容は左寄りになります。")
