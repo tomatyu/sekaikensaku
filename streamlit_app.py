ff05e8f988c52bf1666d3b12f300c34ccef281bd
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-28 02:32:32 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ elif tab == '政治体制検索':
             if not selected_countries.empty:
                 st.subheader(f"{political_system} の国々")
                 # 選択された政治体制に属する国の表を表示
-                st.write(selected_countries["国名"])
+                st.write(selected_countries["国国"])
 
                 # 地図表示
                 st.subheader(f'{political_system} の地図')
